b17878b2ce68f6029beb63a5031598cecd42c2f3
Joaquín Moreno Fernández <joaquin.moreno@grupoesfera.com.ar>
2019-09-26 18:27:49 -0300

    Crea dos subproyectos para emisor y receptor

---
+++ receptor/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+#define RXD2 16
+#define TXD2 17
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
+}
+
+void loop() {
+  while (Serial1.available()) {
+    Serial.print(Serial1.read());
+  }
+}
