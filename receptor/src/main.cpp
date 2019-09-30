c1d173e372fa376a3a7cba840a8225d110872519
Guido Rombolá <guidorombola@yahoo.com.ar>
2019-09-30 09:17:40 -0300

    Agrrega codigo receptor para UART

--- receptor/src/main.cpp
+++ receptor/src/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+ 
+#include <Arduino.h>
+#include <Arduino.h>
+
+#define RXD2 16
+#define TXD2 17
 
 void setup() {
-  Wire.begin(1);                // join i2c bus with address #8
-  Serial.begin(9600);           // start serial for output
+  Serial.begin(115200);
+  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
 
 void loop() {
-  while(Wire.available()){
-    Serial.print(Wire.read());
-    delay(1000);
+  while (Serial1.available()) {
+    Serial.print(Serial1.read());
   }
 }
