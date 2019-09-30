9661ebb5936724aceeb8f0ceae71d3a90b8afb85
Guido Rombolá <guidorombola@yahoo.com.ar>
2019-09-30 10:14:17 -0300

    Crea experimento emisor y receptor variando tamanio de datos

--- emisor/src/main.cpp
+++ emisor/src/main.cpp
@@ -2,14 +2,25 @@
 
 #define RXD2 16
 #define TXD2 17
+#define SENIAL_CONTROL 3
+
+int bytesAEnviar[8] = {2, 20, 200, 2048, 20480, 204800, 2097152, 20971520};
+int indice = 0;
 
 void setup() {
-  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2); 
+  pinMode(SENIAL_CONTROL, OUTPUT);
+  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
 
-void loop() {
-  while (Serial1.available()) {
-    Serial1.write(2);
-    delay(1000);
+void loop(){
+  if(indice < 8){
+    digitalWrite(SENIAL_CONTROL, HIGH);
+    byte buffer[bytesAEnviar[indice]];
+    memset(buffer, 1, bytesAEnviar[indice]);
+    Serial1.write(buffer, bytesAEnviar[indice]);
+    Serial1.flush(); //En teoria, con esto no sigue el programa hasta que no envio todo
+    indice++;
+    digitalWrite(SENIAL_CONTROL, LOW); 
   }
+  delay(2000);
 }
--- receptor/src/main.cpp
+++ receptor/src/main.cpp
@@ -1,20 +1,42 @@
 #include <Arduino.h>
 #include <Wire.h>
 
- 
-#include <Arduino.h>
-#include <Arduino.h>
-
 #define RXD2 16
 #define TXD2 17
+#define INTERRUPT_PIN 2
+
+int tiempos[8];
+int numeroExperimento = 0;
+volatile int tiempoInicial = 0;
+volatile int tiempoFinal = 0;
+int bytesRecibidos = 0;
+
+void iniciarContador(){
+  tiempoInicial = millis();
+}
+
+void detenerContador(){
+  tiempoFinal = millis() - tiempoInicial;
+  tiempos[numeroExperimento] = tiempoFinal;
+  numeroExperimento++;
+  tiempoFinal = 0;
+  tiempoInicial = 0;
+}
 
 void setup() {
+  memset(tiempos, 8, 0);
+  pinMode(INTERRUPT_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), iniciarContador, RISING);
   Serial.begin(115200);
   Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
 
 void loop() {
-  while (Serial1.available()) {
-    Serial.print(Serial1.read());
+  if(digitalRead(INTERRUPT_PIN) == HIGH){
+    while (Serial1.available()) {
+      Serial.print(Serial1.read());
+      bytesRecibidos++;
+    }
+    detenerContador();
   }
 }
