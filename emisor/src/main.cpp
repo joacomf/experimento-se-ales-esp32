3ee83733914f41cb277ae9bf6a21a64d9169137d
Guido Rombolá <guidorombola@yahoo.com.ar>
2019-10-03 21:19:53 -0300

    WIP - Envio y recepcion de datos con buffer

--- emisor/src/main.cpp
+++ emisor/src/main.cpp
@@ -2,25 +2,31 @@
 
 #define RXD2 16
 #define TXD2 17
-#define SENIAL_CONTROL 3
+#define SENIAL_CONTROL 23
+#define CANTIDAD_DE_EXPERIMENTOS 3
 
-int bytesAEnviar[8] = {2, 20, 200, 2048, 20480, 204800, 2097152, 20971520};
+//int bytesAEnviar[8] = {2, 20, 200, 2048, 20480, 204800, 2097152, 20971520};
+int bytesAEnviar[2] = {2, 20};
 int indice = 0;
 
 void setup() {
   pinMode(SENIAL_CONTROL, OUTPUT);
+  Serial.begin(115200);
   Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
 
 void loop(){
-  if(indice < 8){
+  if(indice < CANTIDAD_DE_EXPERIMENTOS){
+    Serial.print("Experimiento ");
+    Serial.println(indice);
     digitalWrite(SENIAL_CONTROL, HIGH);
     byte buffer[bytesAEnviar[indice]];
     memset(buffer, 1, bytesAEnviar[indice]);
+    Serial.println(Serial1.availableForWrite());
     Serial1.write(buffer, bytesAEnviar[indice]);
     Serial1.flush(); //En teoria, con esto no sigue el programa hasta que no envio todo
     indice++;
-    digitalWrite(SENIAL_CONTROL, LOW); 
   }
-  delay(2000);
+  digitalWrite(SENIAL_CONTROL, LOW);
+  delay(100);
 }
--- receptor/src/main.cpp
+++ receptor/src/main.cpp
@@ -30,24 +30,48 @@ void reiniciarExperimento(){
 void setup() {
   memset(tiempos, 0, CANTIDAD_EXPERIMENTOS);
   pinMode(INTERRUPT_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), iniciarContador, RISING);
+  //attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), iniciarContador, RISING);
   Serial.begin(115200);
   Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
 
-void loop() {
-  if(digitalRead(INTERRUPT_PIN) == HIGH && numeroExperimento < CANTIDAD_EXPERIMENTOS){
-    while (Serial1.available()) {
-      Serial1.read();
-      bytesRecibidos++;
+void loop(){
+
+  while(digitalRead(INTERRUPT_PIN) == LOW);
+  iniciarContador();
+  byte buffer[64];
+  while (digitalRead(INTERRUPT_PIN) == HIGH && Serial1.available()) {
+    size_t lectura = Serial1.readBytes(buffer, 64);
+    for (size_t i = 0; i < lectura; i++)
+    {
+        Serial.print(buffer[i]);
     }
-    detenerContador();
-    Serial.print("Bytes recibidos: ");
-    Serial.println(bytesRecibidos);
-    Serial.print("Tiempo experimento [ms]: ");
-    Serial.println(tiempos[numeroExperimento]);
-    Serial.println(numeroExperimento);
-    reiniciarExperimento();
   }
-  delay(4000);
+  detenerContador();
+  delay(500);
 }
+
+// void loopa() {
+//   //if(digitalRead(INTERRUPT_PIN) == HIGH && numeroExperimento < CANTIDAD_EXPERIMENTOS){
+//     while(digitalRead(INTERRUPT_PIN) == LOW && !Serial1.available());
+//     byte buffer[64];
+//     iniciarContador();
+//     while (Serial1.available()) {
+//       Serial.print("aca");
+//       Serial.print(Serial1.read());
+//       bytesRecibidos = Serial1.readBytes(buffer, 64);
+//       for (size_t i = 0; i < bytesRecibidos; i++)
+//       {
+//         Serial.print(buffer[i]);
+//       }
+//     }
+//     detenerContador();
+//     Serial.print("Bytes recibidos: ");
+//     Serial.println(bytesRecibidos);
+//     Serial.print("Tiempo experimento [ms]: ");
+//     Serial.println(tiempos[numeroExperimento]);
+//     Serial.println(numeroExperimento);
+//     reiniciarExperimento();
+//   //}
+//   delay(4000);
+// }
