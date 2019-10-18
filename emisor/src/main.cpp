36f3e547afcd540645c972b36167faf6d47ad9f0
Nacho <ignacioabadie@live.com.ar>
2019-10-18 18:08:33 -0300

    Proy Base de SPI

--- emisor/src/main.cpp
+++ emisor/src/main.cpp
@@ -4,12 +4,9 @@
 #define TXD2 17
 
 void setup() {
-  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2); 
+  
 }
 
 void loop() {
-  while (Serial1.available()) {
-    Serial1.write(2);
-    delay(1000);
-  }
+  
 }
